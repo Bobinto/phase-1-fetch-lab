6adbf208ace4d8717f803733a5cd390d5819be73
bob <Mansalati18@gmail.com>
2024-04-02 11:17:44 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
-
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const bookList = document.getElementById("book-list");
+
+  bookList.innerHTML = "";
+
+  books.forEach((book) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = book.name;
+    bookList.appendChild(listItem);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json()) 
+  .then((books) => {
+
+    renderBooks(books);
+  })
+  .catch((error) => console.error("Error fetching books:", error));
